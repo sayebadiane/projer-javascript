73d5bff78d18804791e9e534d75d904b84779d2b
sbadiane <sbadiane@unikerp.com>
2024-08-01 10:54:03 +0000

    first commit

---
+++ inscription/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Gestion des inscription",
+    'version': '17.0.1.0.0',
+    'depends': ['website'],
+    "author":"Unikerp",
+    "website" : "www.unikerp.com",
+    'description': """ Gestion des inscriptions """,
+    'application': True,
+    'module_type' : 'officiel',
+    'data': [  
+    ],
+}
---
+++ inscription/models/res_partner.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    
